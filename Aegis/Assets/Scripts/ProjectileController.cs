0960571c2b649b3f6a04afd5913f1e1d829ae363
Josh McCoy <jamccoy@ucdavis.edu>
2022-04-27 10:21:36 -0700

    Fixed projectile colors.

--- Aegis/Assets/Scripts/ProjectileController.cs
+++ Aegis/Assets/Scripts/ProjectileController.cs
@@ -7,8 +7,10 @@ using Aegis;
 public class ProjectileController : MonoBehaviour
 {
     [SerializeField] private float Damage = 20.0f;
-    [SerializeField] private float ChargeDelay = 1.0f;
-    [SerializeField] private EffectTypes Type = EffectTypes.Kinetic;
+    [SerializeField] private float chargeDelay = 1.0f;
+    [SerializeField] private EffectTypes type = EffectTypes.Kinetic;
+    [SerializeField] private EffectTypeColors effectTypeColors;
+
     private float ChargeTimer = 0.0f;
     private bool Fired = false;
 
@@ -16,6 +18,8 @@ public class ProjectileController : MonoBehaviour
     {
         this.ChargeTimer = 0f;
         this.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+        this.gameObject.GetComponentInChildren<Renderer>().material.SetColor("_Color", this.effectTypeColors.GetColorByEffectType(this.type));
     }
 
     public float GetDamage()
@@ -25,12 +29,12 @@ public class ProjectileController : MonoBehaviour
 
     public EffectTypes GetEffectType()
     {
-        return this.Type;
+        return this.type;
     }
 
     void Update()
     {
-        if(this.ChargeTimer > this.ChargeDelay && !this.Fired)
+        if(this.ChargeTimer > this.chargeDelay && !this.Fired)
         {
             this.GetComponent<ProjectileMotion>().Fire();
             this.Fired = true;
@@ -38,9 +42,11 @@ public class ProjectileController : MonoBehaviour
         else if(!this.Fired)
         {
             this.ChargeTimer += Time.deltaTime;
-            var scale = this.ChargeTimer / this.ChargeDelay;
+            var scale = this.ChargeTimer / this.chargeDelay;
             //var scale = this.transform.localScale * scale;
             this.transform.localScale = new Vector3(scale, scale, scale);
         }
+
+        this.gameObject.GetComponentInChildren<Renderer>().material.SetColor("_Color", this.effectTypeColors.GetColorByEffectType(this.type));
     }
 }
